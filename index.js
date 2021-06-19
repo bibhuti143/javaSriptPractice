eec63134d546a017578a263ca12fddc9e20a3d42
Bibhuti Rath <rath.bibhuti@gmail.com>
2021-06-19 23:25:02 +0530

    firstCommit

---
+++ index.js
@@ -0,0 +1,19 @@
+// console.log("Hey Out there!!");
+
+//types of JS
+
+// let person = {name : "Bibhuti",
+//               age : 30};  
+
+// let selectedColor = ["red","pink","orange"];
+
+// selectedColor[3]=4;
+
+// console.log(selectedColor.length);
+
+
+function greet(name) {
+    console.log("Hello "+name);
+}
+
+greet("John")
